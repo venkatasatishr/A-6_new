78fc33ff70a639402931cf3335fd9dbddf4b94a3
venkatasatishr <satish.rakonda@india.semanticbits.com>
2019-03-13 19:58:44 +0530

    code using formEvent

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+//const varible contains some static data
+import { allBooks, allReaders} from "./data";
+import {Observable, of, from, fromEvent, concat} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+  source1$;
+  source2$;
+
+  ngOnInit(){
+    this.source1$ = of(10, true, "sam", "good mrng");
+    //this.source1$.subscribe(data => console.log(data));
+    console.log("<<**---------------------------**>>");
+    this.source2$ = from(allBooks);
+    //this.source2$.subscribe(book => console.log(book.title));
+
+    //How to combine two observables ?
+    concat(this.source1$, this.source2$).subscribe(data => console.log(data));
+    //concat returns a single observable that produce all the values from the first observable and second observable
+
+
+  }
 }
